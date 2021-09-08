bbb171170151a68144f7fe7b4520da34d8bdd901
Ruochen <ruochenshi@hotmail.com>
2021-09-08 17:08:14 +0800

    Update DartConfiguration.tcl

--- build/DartConfiguration.tcl
+++ build/DartConfiguration.tcl
@@ -67,9 +67,9 @@ P4UpdateOptions:
 P4UpdateCustom: 
 
 # Generic update command
-UpdateCommand: 
+UpdateCommand: C:/Program Files/Git/cmd/git.exe
 UpdateOptions: 
-UpdateType: 
+UpdateType: git
 
 # Compiler info
 Compiler: C:/Program Files/mingw-w64/x86_64-8.1.0-win32-seh-rt_v6-rev0/mingw64/bin/x86_64-w64-mingw32-g++.exe
